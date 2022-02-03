5b9dd11db8da320add63b26e81a4f95da134f0b0
JakeBarr <97732721+JakeBarr@users.noreply.github.com>
2022-02-03 14:24:14 +0000

    Replacing app.py with app.js

---
+++ templates/app.js
@@ -0,0 +1,13 @@
+function toggleOrder(iconID) {
+    console.log('hellog')
+    let orderNum = iconID.substring(2);
+    if(document.getElementById(iconID).className=="fa fa-toggle-on fa-2x"){
+        document.getElementById(iconID).className = "fa fa-toggle-off fa-2x";
+        document.getElementById("card-head"+orderNum).className = 'card-header todo';
+        document.getElementById("deliver-btn"+orderNum).style.display = "none";
+    }else{
+        document.getElementById(iconID).className = "fa fa-toggle-on fa-2x";
+        document.getElementById("card-head"+orderNum).className = 'card-header prep';
+        document.getElementById("deliver-btn"+orderNum).style.display = "block";
+    }
+}
