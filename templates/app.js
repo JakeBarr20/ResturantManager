662563134f7fb549a4ed874125e4e6d437c45129
JakeBarr <zjac089@live.rhul.ac.uk>
2022-02-06 15:01:16 +0000

    Fixed remove from list feature

--- templates/app.js
+++ templates/app.js
@@ -1,14 +1,24 @@
 function AddToOrder(){
-    tableID = document.getElementById("validationDefault01").value;
+    orderID = document.getElementById("validationDefault01").value;
     item = document.getElementById("validationDefault02").value;
     if (document.getElementById("AddCheckbutton").checked){ 
         var node = document.createElement('li');
         node.className = 'list-group-item';
         node.appendChild(document.createTextNode(item));
-        document.getElementById(tableID).appendChild(node);
-    }else{
-        var l = document.getElementById(tableID).value;
-        l.removeChild(l.childNodes[l.indexOf(item)]);
+        document.getElementById(orderID).appendChild(node);
+    }else if (document.getElementById("RemoveCheckbutton").checked){
+        var list = document.getElementById(orderID);
+        var index = getIndex();
+        list.removeChild(list.childNodes[index]);
     }
-   
+}
+
+function getIndex(){
+    const lis = document.getElementById(orderID).getElementsByClassName('list-group-item');
+        for(var i = 0; i <= lis.length-1; i++) {;
+            if(lis[i].innerHTML == item){
+                return 2*i;
+            }
+        }
+        alert("Item not in list");
 }
