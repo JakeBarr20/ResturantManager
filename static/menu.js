f759a79ece58e74855b2aff41bd2c81d6e21093f
JLEE563V2 <zjac041@live.rhul.ac.uk>
2022-02-11 00:30:46 +0000

    Refactored some of the code, moved the appropriate js and css into files in a separate static folder

---
+++ static/menu.js
@@ -0,0 +1,14 @@
+function popupAllergies() {
+    var Cal = "(Calories here)";
+    var Allergy = "(Allergies here)";
+    var message = "KCal: " + Cal + "\nAllergies: " + Allergy;
+    alert(message);
+  }
+  var data = 0;
+  var x = 0;
+  function increase(id) {
+    document.getElementById("counting").innerText = data;
+    x = document.getElementById(id).value;
+    data = parseFloat(data) + parseFloat(x);
+    document.getElementById("counting").innerText = data;
+  }
