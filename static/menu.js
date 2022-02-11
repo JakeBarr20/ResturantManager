193b5d81851b4c59722af54112e292933096a4ff
JLEE563V2 <zjac041@live.rhul.ac.uk>
2022-02-11 15:33:22 +0000

    Added pictures behind every card of the menu to make it more intuitive

--- static/menu.js
+++ static/menu.js
@@ -8,12 +8,7 @@ let data = 0;
 let x = 0;
 
 function increase(id) {
-  console.log("Called the increase function")
-  console.log(data);
-  document.getElementById("counting").innerText = data;
-  x = document.getElementById(id).value;
-  console.log(document.getElementById(id).value)
-  console.log(x);
+  x = document.getElementById(id).innerHTML;
   data = parseFloat(data) + parseFloat(x);
-  document.getElementById("counting").innerText = data;
+ document.getElementById("counting").innerText = data;
 }
