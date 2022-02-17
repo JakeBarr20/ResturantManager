d88ef2ca39b13abd80bf7d1f8547858f6fdb2659
JakeBarr <zjac089@live.rhul.ac.uk>
2022-02-17 23:17:09 +0000

    Added stacked adds/removes

--- static/WaiterApp.js
+++ static/WaiterApp.js
@@ -1,31 +1,60 @@
 function AddToOrder(){
-    quantity = document.getElementById("quant").value;
-    orderID = document.getElementById("validationDefault01").value;
-    item = document.getElementById("validationDefault02").value;
+    let quantity = document.getElementById("quant").value;
+    let orderID = document.getElementById("validationDefault01").value;
+    let item = document.getElementById("validationDefault02").value;
     if (document.getElementById("AddCheckbutton").checked){ 
-        var node = document.createElement('li');
-        node.className = 'list-group-item';
-        node.appendChild(document.createTextNode(quantity + "x " + item));
-        document.getElementById(orderID).appendChild(node);
-        
+        add(orderID, item, quantity);
         //window.parent.AddItemToDb(item,orderID,quantity); //calls the function for interacting with database
-
     }else if (document.getElementById("RemoveCheckbutton").checked){
-        var list = document.getElementById(orderID);
-        var index = getIndex();
-        list.removeChild(list.childNodes[index]);
+        remove(orderID, item, quantity);
     }
 }
 
-function getIndex(){
-  const lis = document.getElementById(orderID).getElementsByClassName('list-group-item');
+function add(orderID, item, quantity){
+    var list = document.getElementById(orderID);
+    let count = false;
+    const lis = document.getElementById(orderID).getElementsByClassName('list-group-item');
         for(var i = 0; i <= lis.length-1; i++) {;
-            if(lis[i].innerHTML == item){
-                return i;
+            count = false;
+            if((lis[i].innerHTML).substring(3) == item){;
+                quantCard = Number((lis[i].innerHTML).substring(0,1));
+                let q = quantCard + Number(quantity);
+                list.removeChild(list.childNodes[i]);
+                update(orderID, item, q);
+                count = true;
+            }
+            
+        }
+        if (count == false){alert("Item not in list");}
+}
+
+function update(orderID, item, quantity){
+    var node = document.createElement('li');
+    node.className = 'list-group-item';
+    node.appendChild(document.createTextNode(quantity + "x " + item));
+    document.getElementById(orderID).appendChild(node);
+}
+
+function remove(orderID, item, quantity){
+    var list = document.getElementById(orderID);
+    let count = false;
+    const lis = document.getElementById(orderID).getElementsByClassName('list-group-item');
+        for(var i = 0; i <= lis.length-1; i++) {;
+            count = false;
+            if(((lis[i].innerHTML).substring(3) == item) && ((lis[i].innerHTML).substring(0,1) == quantity)){
+                list.removeChild(list.childNodes[i]);
+                count = true;
+            }
+            else if((lis[i].innerHTML).substring(3) == item){
+                quantCard = Number((lis[i].innerHTML).substring(0,1));
+                let q = quantCard - Number(quantity);
+                list.removeChild(list.childNodes[i]);
+                update(orderID, item, q);
+                count = true;
             }
         }
-        alert("Item not in list");
-}
+        if (count == false){alert("Item not in list");}
+}       
 
 
 
