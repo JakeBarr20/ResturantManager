c73545ce8c839c68dc1c59acbdf75dfb676bb297
OwenNorman1 <owen.norman.2020@live.rhul.ac.uk>
2022-02-24 22:39:28 +0000

    Created button in checkout to add default value to database

--- static/checkout.js
+++ static/checkout.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js";
-import { getFirestore, collection, query, where, orderBy, deleteDoc, doc, setDoc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
-
 const firebaseConfig = {
     apiKey: "AIzaSyD_-a7AcOzc2g4awLO2PeneU8enHKBw7cU",
     authDomain: "restaurant-database-92c17.firebaseapp.com",
@@ -11,23 +7,19 @@ const firebaseConfig = {
     appId: "1:673538791861:web:3eb8b2c76a041fa64be1fc",
     measurementId: "G-5PQJ0CN4YQ"
 };
-items = "";
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-console.log(app);
 
-async function addToDatabase() {
-	//Add to database here
+var database = firebase.database();
+
+async function addDoc() {
+	Num = "1";
+	Status = "Waiting"
+	Time = Date()
+	Food ="Cheeseburger x1"
+	database.ref('Orders/' + Num).set({
+		OrderNum : Num;
+		Status : Status;
+		Time : Time;
+		food : Food;
+	});
 }
-
-function addToList() {
-	var Order = document.getElementById('Order').value;
-	items = items + Order + ","
-	var listNode = document.getElementById('OrderList');
-	var newNode = document.createElement("LI");
-	var textNode = document.createTextNode(Order);
-	newNode.appendChild(textNode);
-	listNode.appendChild(newNode);
-	}
-
