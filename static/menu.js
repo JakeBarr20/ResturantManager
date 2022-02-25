b0caa79cf0fb8b1d30f2887a547a1c499f1e4b1f
JLEE563V2 <zjac041@live.rhul.ac.uk>
2022-02-25 09:54:39 +0000

    Committing to show client the upcoming work

--- static/menu.js
+++ static/menu.js
@@ -11,3 +11,27 @@ function increase(id) {
   data = parseFloat(data) + parseFloat(x);
  document.getElementById("counting").innerText = data;
 }
+
+// Make class to activate a css style class that comes over cards when pressed
+$(document).ready(function () {
+  console.log("hello world");
+  $().toggleClass();
+  $("#test1").toggleClass("active");
+});
+
+function availability() {
+  document.getElementById("testCard").style.display = "none";
+}
+
+function popAllergies() {
+  console.log("its running");
+  $(".dissappear").toggleClass("active");
+  $(".appear").toggleClass("active");
+}
+
+$(function testParams() {
+    $('#dissappear').click(function(){
+        let x = $(this).attr('value');
+        console.log(x);
+    });
+});
